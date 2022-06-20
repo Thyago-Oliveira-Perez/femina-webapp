3e70ace71b995ff3e8f16eee24d0f7d2c344c8a2
anna sganderla <sganderlakarlaanna@gmail.com>
2022-06-20 20:07:19 -0300

    add mui

--- src/pages/Home/index.page.tsx
+++ src/pages/Home/index.page.tsx
@@ -1,9 +1,15 @@
 import * as S from './styles';
+import Button from '@mui/material/Button';
 
 export function HomePage() {
     return (
+        <>
         <S.StyledH1>
             Teste
         </S.StyledH1>
+
+        <Button variant="text">Text</Button>
+        </>
+        
     )
 }
