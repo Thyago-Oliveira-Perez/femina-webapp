54c6be1e516aa9a5c677eff932127b0243a10d1d
Maria Julia Furtado <mariajulia12003@gmail.com>
2022-09-13 21:38:36 -0300

    feat-novos componentes Botton e TextField

--- src/pages/Produtos/index.tsx
+++ src/pages/Produtos/index.tsx
@@ -1,10 +1,28 @@
-import * as S from './styles';
 import { ProductCard } from '../../components/ProductCard';
+import{ ButtonComponent } from '../../components/ButtonComponent';
 
-export function Produtos() {
+export function Produtos({}) {
     return (
         <>
             <ProductCard destaque={false} price={100} name={'C3PO'} image={'https://www.comboinfinito.com.br/principal/wp-content/uploads/2021/12/star-wars-eclipse-1-1.jpg'} />
+
+            <ButtonComponent 
+            title={'texto'}
+            type={'button'}
+            themeColor= {'red'}
+            onClick={undefined}
+            disabled={false}
+            width= {'string'}
+            height= {'string'}
+            style = {{}}
+            fullWidth = {true} 
+            border = {true}
+            disableRipple = {false}
+            disablePaddingInline = {false}
+            iconTop = {false}
+            />
         </>
     );
+
+    
 }
