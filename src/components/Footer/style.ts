3bf909c8ab0cf2a011ee94522685c00943e7479d
DSiminski <siminskidayane@gmail.com>
2022-09-13 21:07:53 -0300

    adjustedFooter

--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -1,30 +1,32 @@
-h2, p, a{
-  margin: 0;
-  padding: 0;
-  color: #FFFFFF;
-}
+import styled from 'styled-components';
 
-.Footer{
+export const FooterContainer = styled.footer`
   width: 100%;
   background-color: #7A0000;
   display: flex;
   align-items: center;
   justify-content: space-evenly;
-}
+  position: absolute;
+  bottom: 0;
+`;
 
-.footer-section{
+export const footerSections = styled.div`
   display: flex;
   align-items: flex-start;
   justify-content: center;
   flex-direction: column;
-}
+`;
 
-h2{
+export const h2 = styled.h2`
   font-size: 15px;
   padding: 10px 0;
-}
+  color: #FFFFFF;
+`;
 
-p{
+export const p = styled.p`
   padding: 6px 0;
   font-size: 10px;
-}
+  color: #FFFFFF;
+`;
+
+
